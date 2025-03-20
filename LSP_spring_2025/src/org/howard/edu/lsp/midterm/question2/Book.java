0b8c9eb17f7d095838ce6526d1b7dadcbf67962b
iniayolawal <inioluwa.ayo-lawal@bison.howard.edu>
2025-03-20 19:39:54 -0400

    code works so far

--- LSP_spring_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
+++ LSP_spring_2025/src/org/howard/edu/lsp/midterm/question2/Book.java
@@ -12,6 +12,15 @@ public class Book {
     private String ISBN;
     private int yearPublished;
     
+    
+    /**
+     * Constructor to initialize book attributes
+     * 
+     * @param title - The title of the book.
+     * @param author - The author of the book.
+     * @param ISBN - The unique ISBN identifier of the book.
+     * @param yearPublished - The year the book was published.
+     */
     public Book(String title, String author, String ISBN, int yearPublished) {
     	this.title = title;
         this.author = author;
@@ -20,43 +29,57 @@ public class Book {
     	
     }
     
-    
+    // Getter for title
     public String getTitle() {
         return title;
     }
     
+    
+    // Getter for author
     public String getAuthor() {
         return author;
     }
     
+    // Getter for ISBN
     public String getISBN() {
         return ISBN;
     }
+    
+    // Getter for year published
     public int getYearPublished() {
         return yearPublished;
     }
     
-    @Override
+    
+    // Override equals method to compare books based on ISBN and author
+    //
+    @Override // overiding in java https://www.geeksforgeeks.org/overriding-in-java/#
     public boolean equals(Object obj) {
         if (this == obj) return true; // Check if comparing to itself
         if (!(obj instanceof Book)) return false; // Ensure the object is a Book
         Book other = (Book) obj;
         return this.ISBN.equals(other.ISBN) && this.author.equals(other.author);
     }
+    
+    
+    // Override toString method to provide a readable book description
+    @Override // overiding in java https://www.geeksforgeeks.org/overriding-in-java/#
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Year Published: " + yearPublished;
+    }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ 
+    // Main method for testing the Book class
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+        Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book2 = new Book("The Great Gatsby", "F. Scott Fitzgerald", "123456789", 1925);
+        Book book3 = new Book("A Promised Land", "Barack Obama", "9780593239681", 2020);
+        System.out.println(book1.equals(book2)); // true
+        System.out.println(book1.equals(book3)); // false
+        System.out.println(book1); // Output: Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456789, Year Published: 1925
+
 	}
 
 }
